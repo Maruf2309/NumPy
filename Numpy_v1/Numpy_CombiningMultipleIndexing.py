747949f0a8634906859addb65facda98ec9f457d
Maruf Hossain <102369168+Maruf2309@users.noreply.github.com>
2024-11-08 17:35:45 +0000

    code up

---
+++ Numpy_v1/Numpy_CombiningMultipleIndexing.py
@@ -0,0 +1,30 @@
+
+# Example 30: NumPy - Combining Multiple Indexing Techniques
+import numpy as np
+
+# Creating a 2D array
+array = np.array([[10, 20, 30], [40, 50, 60], [70,80,90]])
+
+# Combining boolean, fancy, and slice indexing
+result = array[array > 30][:, [0, 2]]
+
+# print('Combined Indexing Result:', result)
+
+# Creating a boolean mask
+mask = array > 30
+
+# Applying the mask and reshaping to the original array shape (for demonstration) 
+filtered_array = np.where(mask, array, np.nan)
+
+# Selecting the intended elements (fancy indexing)
+
+result = filtered_array[:, [0, 2]] 
+print('Combined Indexing Result:') 
+print(result)
+
+
+
+
+
+
+
---
+++ Numpy_v1/Numpy_ConcatenationArray.py
@@ -0,0 +1,11 @@
+# Example 15: NumPy - Concatenation of Arrays
+import numpy as np
+
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+# Concatenation
+concatenation_array = np.concatenate((array1, array2)) # its requrid duble paranthesic
+
+# Printing
+print("Concatanted Array :", concatenation_array)
